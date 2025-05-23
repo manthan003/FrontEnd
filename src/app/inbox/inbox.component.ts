e7d2b0292bfe738e9b3994e5cc5d33ff75a9a580
CRManthan <c.r.manthan@ibm.com>
2025-05-23 11:08:40 +0530

    Final

--- src/app/inbox/inbox.component.ts
+++ src/app/inbox/inbox.component.ts
@@ -1,4 +1,3 @@
-// src/app/inbox/inbox.component.ts
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../contact/contact.service';
 import { ContactMessage } from '../models/contact-message.model';
@@ -14,6 +13,15 @@ export class InboxComponent implements OnInit {
   constructor(private contactService: ContactService) {}
 
   ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages(): void {
     this.messages = this.contactService.getMessages();
+  }
+
+  deleteMessage(index: number): void {
+    this.messages.splice(index, 1); // remove from local array
+    localStorage.setItem('contactMessages', JSON.stringify(this.messages)); // update localStorage
   }
 }
